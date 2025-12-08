81a7551907d4316df5e386df46592323536c1806
Roronoa-Zoro-007 <strawhats.rz@gmail.com>
2025-12-09 00:06:04 +0530

    Changes in Registration Service

---
+++ src/main/java/com/wishit/auth/dto/RegistrationDTO.java
@@ -0,0 +1,79 @@
+package com.wishit.auth.dto;
+
+public class RegistrationDTO {
+	private Name name;
+    private Integer age;
+    private String email;
+    private Long mobileNo;
+    private String gender;
+    private String city;
+    private String password;
+    
+	public Name getName() {
+		return name;
+	}
+	public void setName(Name name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(Long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
+
+
+class Name{
+	private String firstName;
+    private String middleName;
+    private String lastName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}  
+}
--- src/main/java/com/wishit/auth/service/RegistrationService.java
+++ src/main/java/com/wishit/auth/service/RegistrationService.java
@@ -1,8 +1,9 @@
 package com.wishit.auth.service;
 
-
 import org.springframework.stereotype.Service;
 
+import com.wishit.auth.dto.ProfileDTO;
+import com.wishit.auth.dto.RegistrationDTO;
 import com.wishit.auth.entity.Registration;
 import com.wishit.auth.repository.RegistrationRepo;
 
@@ -18,14 +19,26 @@ public class RegistrationService {
 	        this.userProfileService = userProfileService;
 	    }
 
-	    public Registration registerUser(Registration user) {
+	    public Registration registerUser(RegistrationDTO regDTO) {
 	      
-	    	if (repository.findByEmailAndPassword(user.getEmail(), user.getPassword()).isPresent()) {
+	    	if (repository.findByEmailAndPassword(regDTO.getEmail(), regDTO.getPassword()).isPresent()) {
 	    	    throw new RuntimeException("Email already in use");
 	    	    
 	    	}
-
-	        Registration savedUser = repository.save(user);
+	    	
+	    	Registration newUser = new Registration();
+	    	newUser.setEmail(regDTO.getEmail());
+	    	newUser.setName(regDTO.getName().toString());
+	    	newUser.setPassword(regDTO.getPassword());
+	    	
+	    	ProfileDTO profileDTO = new ProfileDTO();
+	    	profileDTO.setAge(regDTO.getAge());
+	    	profileDTO.setCity(regDTO.getCity());
+	    	profileDTO.setGender(regDTO.getGender());
+	    	profileDTO.setMobileNo(regDTO.getMobileNo());
+	    	profileDTO.setFirstName(regDTO.getName());
+	    	
+	        Registration savedUser = repository.save(newUser);
 	        userProfileService.addUserProfile(null, savedUser);
 	        return savedUser;
 	    }
